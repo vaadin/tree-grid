ce337c660c83b96d900f3529a003953b6d000df2
adam <adam@vaadin.com>
2016-09-14 16:08:51 +0300

    Removed keyboard navigation to be able to compile without fw patch

--- treegrid-addon/src/main/java/org/vaadin/treegrid/client/TreeGrid.java
+++ treegrid-addon/src/main/java/org/vaadin/treegrid/client/TreeGrid.java
@@ -6,9 +6,9 @@ import elemental.json.JsonObject;
 
 public class TreeGrid extends Grid<JsonObject> {
 
-    /* Override to make package accessible */
-    @Override
-    protected void focusCell(int rowIndex, int columnIndex) {
-        super.focusCell(rowIndex, columnIndex);
-    }
+//    /* Override to make package accessible */
+//    @Override
+//    protected void focusCell(int rowIndex, int columnIndex) {
+//        super.focusCell(rowIndex, columnIndex);
+//    }
 }
