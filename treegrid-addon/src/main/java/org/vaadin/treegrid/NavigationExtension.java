2c15f8bd3215bd0d6a29a616d87b665428680891
adam <adam@vaadin.com>
2016-09-22 14:37:46 +0300

    Language level 7

--- treegrid-addon/src/main/java/org/vaadin/treegrid/NavigationExtension.java
+++ treegrid-addon/src/main/java/org/vaadin/treegrid/NavigationExtension.java
@@ -6,16 +6,16 @@ import org.vaadin.treegrid.client.NavigationExtensionConnector;
 
 public class NavigationExtension extends Grid.AbstractGridExtension {
 
-    private NavigationExtension(TreeGrid grid) {
+    private NavigationExtension(final TreeGrid grid) {
         super(grid);
 
-//        registerRpc(new NavigationExtensionConnector.NodeCollapseRpc() {
-//            @Override
-//            public void toggleCollapse(String rowKey) {
-//                Object itemId = getItemId(rowKey);
-//                grid.toggleExpansion(itemId);
-//            }
-//        });
+        registerRpc(new NavigationExtensionConnector.NodeCollapseRpc() {
+            @Override
+            public void toggleCollapse(String rowKey) {
+                Object itemId = getItemId(rowKey);
+                grid.toggleExpansion(itemId);
+            }
+        });
     }
 
     public static NavigationExtension extend(TreeGrid grid) {
